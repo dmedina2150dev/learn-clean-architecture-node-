51429ca5627af5ef2e88ea9fca050423b8d9c4f0
Dajan Medina <dmedina2150dev@gmail.com>
2023-11-25 01:35:58 -0300

    Creamos la implemntacion del repository

---
+++ node-auth/src/infrastructure/repositories/auth.repository.impl.ts
@@ -0,0 +1,16 @@
+/**
+ * 
+*/
+import { AuthDatasource, RegisterUserDto, UserEntity, AuthRepository } from '../../domain';
+
+export class AuthRepositoryImpl implements AuthRepository {
+
+    constructor(
+        private readonly authDatasource: AuthDatasource
+    ) {}
+    
+    register(registerUserDto: RegisterUserDto): Promise<UserEntity> {
+        return this.authDatasource.register(registerUserDto);
+    }
+
+}
