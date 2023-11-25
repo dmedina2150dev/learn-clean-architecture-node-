ac7ee740eb7c753541eb222e91dfb3024ad95e68
Dajan Medina <dmedina2150dev@gmail.com>
2023-11-25 17:57:38 -0300

    Creamos la conexicion a la base de datos

--- node-auth/src/app.ts
+++ node-auth/src/app.ts
@@ -1,4 +1,5 @@
 import { envs } from './configs';
+import { MongoDatabase } from './data/mongodb';
 
 import { AppRoutes } from './presentation/routes';
 import { Server } from './presentation/server';
@@ -9,9 +10,13 @@ import { Server } from './presentation/server';
 
 async function main () {
     // TODO: await de la base de datos
+    await MongoDatabase.connect({
+        dbName: envs.MONGO_DB_NAME,
+        mongoUrl: envs.MONGO_URL
+    });
     
     // TODO: inicio del servidor
-    console.log("main");
+    // console.log("main");
     new Server({
         port: envs.PORT,
         routes: AppRoutes.routes,
