4d9081a101314d7d887ba7ebd137f2b260cf989d
Dajan Medina <dmedina2150dev@gmail.com>
2023-11-27 20:47:18 -0300

    Creamos un usuario en la base de datos

--- node-auth/src/infrastructure/datasources/auth.datasource.impl.ts
+++ node-auth/src/infrastructure/datasources/auth.datasource.impl.ts
@@ -2,6 +2,7 @@
  * 
  */
 
+import { UserModel } from "../../data/mongodb";
 import { AuthDatasource, CustomError, RegisterUserDto, UserEntity } from "../../domain";
 
 export class AuthDatasourceImpl implements AuthDatasource {
@@ -12,17 +13,31 @@ export class AuthDatasourceImpl implements AuthDatasource {
         try {
 
             // 1. Verificar si existe correo en base de datos
-
+            const existEmail = await UserModel.findOne({ email: email });
+            if (existEmail) throw CustomError.badRequest(`User ${email} already`);
+            
             // 2. Encriptar constraseña
 
-            // 3. Mepear las respuestas a nuestra entidad
+            
+            // 2.1 Crear el usuario que se guardara
+            const user = await UserModel.create({
+                name: name,
+                email: email,
+                password: password
+            });
 
+            // 2.2 Guardar el usuario en la base de datos
+            await user.save();
+
+
+            // 3. Mepear las respuestas a nuestra entidad
+            // TODO: Falta hacer un mapper
             return new UserEntity(
-                '1',
+                user.id,
                 name,
                 email,
                 password,
-                ['ADMIN'],
+                user.roles,//['ADMIN'],
             )
             
         } catch (error) {
