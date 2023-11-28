5846e03e06070b79dd1562ce1d2270486bccff02
Dajan Medina <dmedina2150dev@gmail.com>
2023-11-27 21:41:26 -0300

    Inyectamos la dependencia para encriptar

--- node-auth/src/infrastructure/datasources/auth.datasource.impl.ts
+++ node-auth/src/infrastructure/datasources/auth.datasource.impl.ts
@@ -1,12 +1,21 @@
 /**
- * 
+ * ? Para  que las importaciones dentro del constructor se vean mas claras ⤵️
+ * ? Al momento de que sea una funcion, recomendacion crear un Type de TypeScript
  */
 
 import { BcryptAdapter } from "../../configs";
 import { UserModel } from "../../data/mongodb";
 import { AuthDatasource, CustomError, RegisterUserDto, UserEntity } from "../../domain";
 
+type HashFunction = (password: string) => string;
+type CompareFunction = (password: string, hashed: string) => boolean;
+
 export class AuthDatasourceImpl implements AuthDatasource {
+
+    constructor(
+        private readonly hashPassword: HashFunction = BcryptAdapter.hash,
+        private readonly comparePassword: CompareFunction = BcryptAdapter.compare,
+    ) {}
 
     async register(registerUserDto: RegisterUserDto): Promise<UserEntity> {
         const { name, email, password } = registerUserDto;
@@ -24,7 +33,7 @@ export class AuthDatasourceImpl implements AuthDatasource {
             const user = await UserModel.create({
                 name: name,
                 email: email,
-                password: BcryptAdapter.hash(password)
+                password: this.hashPassword(password)
             });
 
             // 2.2 Guardar el usuario en la base de datos
