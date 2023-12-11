ebe8dae14d142659820315f2d3b705ca0b29a93b
Dajan Medina <dmedina2150dev@gmail.com>
2023-12-10 22:10:28 -0300

    Creamos los casos de uso y separamos la logica de crear usuario

--- node-auth/src/domain/index.ts
+++ node-auth/src/domain/index.ts
@@ -2,5 +2,6 @@ export * from './dtos';
 export * from './entities';
 export * from './datasources';
 export * from './repositories';
+export * from './use-cases';
 
 export * from './errors/custom.error';
--- node-auth/src/presentation/auth/controller.ts
+++ node-auth/src/presentation/auth/controller.ts
@@ -3,7 +3,7 @@
  */
 
 import { Request, Response } from 'express';
-import { AuthRepository, CustomError, RegisterUserDto } from '../../domain';
+import { AuthRepository, CustomError, RegisterUser, RegisterUserDto } from '../../domain';
 import { JwtAdapter } from '../../configs';
 import { UserModel } from '../../data/mongodb';
 
@@ -30,14 +30,10 @@ export class AuthController {
 
 		if ( error ) return res.status(400).json({ error });
 
-		this.authRepository.register(registerUserDto!)
-			.then( async (user) => {
-				res.json({
-					user,
-					token: await JwtAdapter.generateToken({ id: user.id })
-				});
-			})
-			.catch( error => this.handleError(error, res));
+		new RegisterUser(this.authRepository)
+			.execute(registerUserDto!)
+			.then(data => res.json(data))
+			.catch(error => this.handleError(error, res))
 	}
 	
 	loginUser = (req: Request, res: Response) => {
@@ -48,7 +44,7 @@ export class AuthController {
 		UserModel.find()
 			.then( (users) => {
 				res.json({
-					// users,
+					users,
 					user: req.body.user
 				});
 			})
